784de8a752410143ae3e60e8fc154a8752815e86
asyrawi <asyrawii@gmail.com>
2022-04-09 23:15:30 +0800

    Ways of displaying message

--- Tutorial2/Program.cs
+++ Tutorial2/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Tutorial2
 {
-    class Program
+    class Rectangle
+    {
+
+        // member variables
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            // Ways of displaying 
+            Console.WriteLine("Length and width: {0} {1}", length, width);
+            Console.WriteLine($"Width: {width}");
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+    class ExecuteRectangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
         }
     }
 }
