ba882eaa851415ff2d42081bd3b4b67f0f61a900
asyrawi <asyrawii@gmail.com>
2022-04-10 22:28:54 +0800

    Array done

---
+++ Tutorial3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tutorial3
+{
+    class EnumProgram
+    {
+        enum Days { Sun, Mon, tue, Wed, thu, Fri, Sat };
+
+        static void Main(string[] args)
+        {
+            
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+
+            int[,] array2d = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } }; //int[,] array2d = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            Console.WriteLine(array2d[0,1]);
+
+
+            for (int i = 0; i < array2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                {
+                    Console.WriteLine("Print i and j: {0} {1}", i, j);
+                    Console.WriteLine(array2d[i, j]);
+
+                }
+            }
+
+            int WeekdayStart = (int)Days.Mon;
+            int WeekdayEnd = (int)Days.Fri;
+            Console.WriteLine("Monday: {0}", WeekdayStart);
+            Console.WriteLine("Friday: {0}", WeekdayEnd);
+            Console.ReadKey();
+        }
+    }
+}
